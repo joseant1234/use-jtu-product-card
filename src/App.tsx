b537d6c87de36131af91b4430f42bf25c2124d8e
Jose <jose.antonio.umeres@gmail.com>
2022-10-12 21:07:00 -0500

    Added jtu-product-card lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ProductButtons, ProductCard, ProductTitle, ProductImage } from 'jtu-product-card';
+
+const product = {
+  id: '1',
+  title: 'Coffee Mug - Card',
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <ProductCard
+        product={product}
+        initialValues={{
+          count: 5,
+          maxCount: 10,
+        }}
+      >
+        {
+          ({ reset, count, isMaxCountReached, maxCount, increaseBy }) => (
+              <>
+                <ProductImage />
+                <ProductTitle  />
+                <ProductButtons  />
+              </>
+          )
+        }
+      </ProductCard>
     </div>
   );
 }
